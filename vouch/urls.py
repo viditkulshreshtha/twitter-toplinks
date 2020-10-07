06285c55358045ba1da2de4c9b34775a90970532
viditkulshreshtha <33063443+viditkulshreshtha@users.noreply.github.com>
2020-10-07 20:03:39 +0530

    Update urls.py

--- vouch/urls.py
+++ vouch/urls.py
@@ -4,7 +4,7 @@ from twitterapp import views
 from django.conf.urls import url  
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^home/$', views.homeview, name='home'),
+    url('', views.homeview, name='home'),
     url(r'^username/$', views.usernamereq, name='username'),
     url(r'^tweets/$', views.tweets, name='tweets'), 
     url(r'^friendtweets/$', views.friendstweet, name='friendstweet'),       
